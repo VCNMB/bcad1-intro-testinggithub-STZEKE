8fcc059e78297781c671a141019b24a3b776d8f1
STZEKE <160749218+STZEKE@users.noreply.github.com>
2025-03-04 11:27:40 +0200

    Added Project
    Added prjClasses Project

---
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjclasses;
+
+/**
+ *
+ * @author Codyg
+ */
+public class PrjClasses {
+
+    public static void main(String[] args) {
+        
+    }
+}
